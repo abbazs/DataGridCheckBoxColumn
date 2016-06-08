b3737cda2c10f4721cb05e5c85dfb8d7c21418aa
abbazs <abbazs@gmail.com>
2016-06-08 12:18:16 +0530

    Initial commit

---
+++ DataGridColIssue/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace DataGridColIssue
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int CurrentOperation;
+        public List<Operation> Operations;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Operations = new List<Operation>();
+            CurrentOperation = 0;
+        }
+
+        private void AddItems_Click(object sender, RoutedEventArgs e)
+        {
+            CurrentOperation++;
+            Operations.Add(new Operation(CurrentOperation));
+            DataGridTesting.Items.Refresh();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           DataGridTesting.ItemsSource = Operations;
+           DataGridTesting.Items.Refresh();
+        }
+    }
+}
---
+++ DataGridColIssue/Operation.cs
@@ -0,0 +1,14 @@
+﻿
+namespace DataGridColIssue
+{
+    public class Operation
+    {
+        public bool Exclude { get; set; }
+        public int Number { get; set; }
+
+        public Operation(int Number)
+        {
+            this.Number = Number;
+        }
+    }
+}
